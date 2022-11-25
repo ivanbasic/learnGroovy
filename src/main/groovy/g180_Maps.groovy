280c92184c7b38384b8b6ecca0e6462707cbeb7d
Ivan Basic <ivanbasic@ns>
2022-11-25 14:56:37 +0100

    maps

--- src/main/groovy/g180_Maps.groovy
+++ src/main/groovy/g180_Maps.groovy
@@ -1,8 +1,26 @@
-class g180_Maps implements  aGroovyLesson {
+class g180_Maps implements aGroovyLesson {
 
     def lesson() {
-        def map1 = ['key1':"value", "key2":"value" ]
+        def map1 = ['key1': "value", "key2": "value"]
         def map2 = [:]
 
+        def mapElement =  MapElement.newElement("element3")
+        def map3 = [1:mapElement, 2: "string"]
+    }
+}
+
+class MapElement {
+    def element1
+    def element2
+    String element3
+    int element4
+
+    static MapElement newElement(String element3) {
+        def toReturn = new MapElement()
+        toReturn.setElement1("1")
+        toReturn.setElement2("2")
+        toReturn.setElement3(element3)
+        toReturn.setElement4(1)
+        return toReturn
     }
 }
